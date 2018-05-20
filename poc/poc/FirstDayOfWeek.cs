4bcb03cb1d564d5e9c2fb36ea953f7cb1afdf09b
Elena Pérez-Toril <elenaptg@gmail.com>
2018-05-20 11:45:30 +0200

    List appointments of current week

---
+++ poc/poc/FirstDayOfWeek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+public static class FirstDayOfWeek
+{
+    /// <summary>
+    /// Returns the first day of the week that the specified date 
+    /// is in. 
+    /// </summary>
+    public static DateTime GetFirstDayOfWeek(DateTime dayInWeek, CultureInfo cultureInfo)
+    {
+        DayOfWeek firstDay = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+        DateTime firstDayInWeek = dayInWeek.Date;
+        while (firstDayInWeek.DayOfWeek != firstDay)
+            firstDayInWeek = firstDayInWeek.AddDays(-1);
+
+        return firstDayInWeek;
+    }
+    /// <summary>
+    /// Returns the first day of the week that the specified
+    /// date is in using the current culture. 
+    /// </summary>
+    public static DateTime GetFirstDayOfWeek(DateTime dayInWeek)
+    {
+        CultureInfo defaultCultureInfo = CultureInfo.CurrentCulture;
+        return GetFirstDayOfWeek(dayInWeek, defaultCultureInfo);
+    }
+
+
+}
---
+++ poc/poc/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Exchange.WebServices.Data;
+namespace poc
+{
+    class Program
+    {
+        
+        private ExchangeService Service
+        {
+            get
+            {
+                ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010);
+                service.Credentials = new WebCredentials("email@email.com", "password");
+                service.Url = new Uri("https://outlook.office365.com/ews/exchange.asmx");
+                return service;
+            }
+        }
+
+        private CalendarFolder FindDefaultCalendarFolder()
+        {
+            return CalendarFolder.Bind(Service, WellKnownFolderName.Calendar, new PropertySet());
+        }
+
+        private void LoadAppointments()
+        {
+            DateTime startDate = FirstDayOfWeek.GetFirstDayOfWeek(DateTime.Now);
+            DateTime endDate = startDate.AddDays(7);
+
+            CalendarFolder calendar = FindDefaultCalendarFolder();
+
+            CalendarView cView = new CalendarView(startDate, endDate, 50);
+            cView.PropertySet = new PropertySet(AppointmentSchema.Subject, AppointmentSchema.Start, AppointmentSchema.End, AppointmentSchema.Id);
+            FindItemsResults<Appointment> appointments = calendar.FindAppointments(cView);
+
+            foreach (Appointment appointment in appointments)
+            {
+                Console.WriteLine("Subject: " + appointment.Subject);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.LoadAppointments();
+
+            Console.ReadKey();
+        }
+    }
+}
